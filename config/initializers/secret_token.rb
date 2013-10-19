b86d5a8a3080b656c42c6308df6ba1e4d580f45e
zestos <zestos@gmail.com>
2013-10-19 10:31:27 -0700

    Fresh new Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Doorman::Application.config.secret_token = '89a5dcdfb6d292533c6486662eddada3d8b297415c681d505ccd96bdb078f71c3c0c69054809d3660bf2409ca06d05b0db6bd966279fd791a7068c998a4002dc'
