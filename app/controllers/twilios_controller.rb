f33de7a591704678fe16cfb82e27d4700ade08fd
zestos <zestos@gmail.com>
2013-10-21 16:00:10 -0700

    Playing with dtmf tones

--- app/controllers/twilios_controller.rb
+++ app/controllers/twilios_controller.rb
@@ -1,11 +1,10 @@
 class TwiliosController < ApplicationController
 
   def incoming
-    # Twilio::TwiML::Response.new do |r|
-    #   r.Play 'http://demo.twilio.com/hellomonkey/monkey.mp3'
-    # end.text
-    render :action => "incoming.xml.builder", :layout => false
 
+    @name = "Luke"
+      
+    render action: "incoming.xml.builder", :layout => false
 
   end
 
--- app/views/twilios/incoming.xml.builder
+++ app/views/twilios/incoming.xml.builder
@@ -1,4 +1,5 @@
 xml.instruct!
 xml.Response do
-  xml.Say "Hello monkey"
+  xml.Say "Hello #{@name}"
+  xml.Play "#{Rails.root.join('app', 'tones', '0.mp3')}"
 end
