8d6532a827d69d9e079a6c362471a171e26a5aaf
zestos <zestos@gmail.com>
2013-10-21 09:24:22 -0700

    twiml changes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    @text = Twilio::TwiML::Response.new do |r|
+    @response = Twilio::TwiML::Response.new do |r|
       r.Say 'Hello Monkey'
     end.text
   end
