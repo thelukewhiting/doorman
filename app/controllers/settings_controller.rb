7d398be1e0412c9f54db6f36632eadfe813f26d2
zestos <zestos@gmail.com>
2013-10-23 16:15:04 -0700

    Modified Edit Settings link for new users without settings

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -4,11 +4,25 @@ class SettingsController < ApplicationController
 
   def index
 
+    if current_user.setting != nil
+
+      @message = current_user.setting.message
+
+      @recipient = current_user.setting.recipient
+
       if current_user.setting.autounlock
         @status = "enabled"
       else
         @status = "disabled"
       end
+    
+    else
+
+      @message = "Not set"
+      @recipient = "Not set"
+      @status = "Not set"
+
+    end
   end
 
   def new 
