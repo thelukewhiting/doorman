d2840115ff06fd88b32ef4b64714ec490a6cee2a
zestos <zestos@gmail.com>
2013-10-28 18:38:42 -0700

    Added a few settings controller tests

--- spec/controllers/settings_controller_spec.rb
+++ spec/controllers/settings_controller_spec.rb
@@ -3,9 +3,18 @@ require 'spec_helper'
 describe SettingsController do
   
   describe "GET #new" do
-    it "should not render the #new view if not logged in" do
+    it "should not render #new if not authenticated" do
       get :new 
       response.should_not render_template :new
     end
   end  
+
+  describe "GET #edit" do
+    it "should not render #edit if not authenticated" do
+      get :edit, id: 1
+      response.should_not render_template :edit
+    end
+  end
+
+
 end
